007bc0a3075de34cb196b7cc4113badc9035fda6
lucaslam704 <161893709+lucaslam704@users.noreply.github.com>
2025-01-28 15:51:15 -0700

    add title & link on vercel week 2

--- app/page.js
+++ app/page.js
@@ -2,8 +2,9 @@
 export default function Home() {
   return (
     <div>
+      <title>Cprg306-Assignment</title>
     <h1>CPRG 306: Web Development 2 - Assignments</h1>
-    <h2> <a href="http://localhost:3000/week-2" className="underline underline-offset-4">Press here to proceed Week 2!</a></h2>
+    <h2> <a href="https://cprg306-assignments-alpha-five.vercel.app/week-2" className="underline underline-offset-4">Press here to proceed Week 2!</a></h2>
     </div>
   );
 }
--- app/week-2/page.js
+++ app/week-2/page.js
@@ -3,6 +3,7 @@ import StudentInfo from "./student-info.js";
 export default function Page() {
     return (
       <main>
+        <title>Week 2</title>
         <h1> Shopping List</h1>
         <StudentInfo/>
       </main>
